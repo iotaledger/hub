7ba0ac886af558679a4db4422493144eced1d0cd
Andreas C. Osowski <andreas@osowski.de>
2018-04-21 23:54:42 +0200

    hub/crypto: LocalProvider optimisations

--- hub/crypto/local_provider.cc
+++ hub/crypto/local_provider.cc
@@ -2,7 +2,8 @@
 
 #include <array>
 #include <cstdint>
-#include <iostream>
+#include <functional>
+#include <memory>
 #include <stdexcept>
 
 #include <argon2.h>
@@ -11,6 +12,12 @@
 #include "common/trinary/trits.h"
 #include "common/trinary/tryte.h"
 
+// FIXME (th0br0) fix up entangled
+extern "C" {
+void trits_to_trytes(trit_t*, tryte_t*, size_t);
+void trytes_to_trits(tryte_t*, trit_t*, size_t);
+}
+
 namespace {
 static constexpr size_t BYTE_LEN = 48;
 static constexpr size_t TRIT_LEN = 243;
@@ -18,12 +25,38 @@ static constexpr size_t TRYTE_LEN = 81;
 
 static constexpr size_t KEY_IDX = 0;
 static constexpr size_t KEY_SEC = 2;
-}  // namespace
 
-extern "C" {
-void trits_to_trytes(trit_t*, tryte_t*, size_t);
-void trytes_to_trits(tryte_t*, trit_t*, size_t);
+static constexpr uint32_t _argon_t_cost = 1;
+static constexpr uint32_t _argon_m_cost = 1 << 16;  // 64mebibytes
+static constexpr uint32_t _argon_parallelism = 1;
+
+using TryteSeed = std::array<tryte_t, TRYTE_LEN>;
+using TryteSeedPtr =
+    std::unique_ptr<TryteSeed, std::function<void(TryteSeed*)>>;
+
+TryteSeedPtr seedFromUUID(const boost::uuids::uuid& uuid,
+                          const std::string& _salt) {
+  std::array<uint8_t, BYTE_LEN> byteSeed;
+  std::array<trit_t, TRIT_LEN> seed;
+
+  TryteSeedPtr tryteSeed(new TryteSeed{}, [](TryteSeed* seed) {
+    seed->fill(0);
+    delete seed;
+  });
+
+  argon2i_hash_raw(_argon_t_cost, _argon_m_cost, _argon_parallelism, &uuid.data,
+                   uuid.size(), _salt.c_str(), _salt.length(), byteSeed.data(),
+                   BYTE_LEN);
+
+  bytes_to_trits(byteSeed.data(), seed.data());
+  byteSeed.fill(0);
+
+  trits_to_trytes(seed.data(), tryteSeed->data(), TRIT_LEN);
+  seed.fill(0);
+
+  return tryteSeed;
 }
+}  // namespace
 
 namespace hub {
 namespace crypto {
@@ -39,24 +72,8 @@ LocalProvider::LocalProvider(std::string salt) : _salt(std::move(salt)) {
 
 std::string LocalProvider::getAddressForUUID(
     const boost::uuids::uuid& uuid) const {
-  std::array<uint8_t, BYTE_LEN> byteSeed;
-  std::array<trit_t, TRIT_LEN> seed;
-  std::array<tryte_t, TRYTE_LEN> tryteSeed;
-
-  argon2i_hash_raw(_argon_t_cost, _argon_m_cost, _argon_parallelism, &uuid.data,
-                   uuid.size(), _salt.c_str(), _salt.length(), byteSeed.data(), BYTE_LEN);
-  
-  bytes_to_trits(byteSeed.data(), seed.data());
-  byteSeed.fill(0);
-
-  trits_to_trytes(seed.data(), tryteSeed.data(), TRIT_LEN);
-  seed.fill(0);
-
-  std::string address(
-      iota_sign_address_gen((const char*)tryteSeed.data(), KEY_IDX, KEY_SEC));
-  tryteSeed.fill(0);
-
-  return address;
+  return {iota_sign_address_gen((const char*)seedFromUUID(uuid, _salt)->data(),
+                                KEY_IDX, KEY_SEC)};
 }
 }  // namespace crypto
 }  // namespace hub
