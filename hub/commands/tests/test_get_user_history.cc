08d25076c637596a3bdfc31d712702d2635c583a
tsvisabo <tsvi.sabo@iota.org>
2018-05-01 17:54:41 +0300

    hub/iota: implement getUserHistory command for retreiving user's actions

---
+++ hub/commands/tests/test_get_user_history.cc
@@ -0,0 +1,59 @@
+//
+// Created by tsvi on 5/1/18.
+//
+
+// Copyright 2018 IOTA Foundation
+
+#include <gtest/gtest.h>
+#include <sqlpp11/functions.h>
+#include <sqlpp11/select.h>
+
+#include "proto/hub.pb.h"
+#include "schema/schema.h"
+
+#include "hub/commands/get_user_history.h"
+#include "hub/db/db.h"
+#include "hub/stats/session.h"
+
+#include "runner.h"
+
+using namespace hub;
+using namespace sqlpp;
+
+namespace {
+class GetUserHistoryTest : public CommandTest {};
+
+TEST_F(GetUserHistoryTest, UnknownUserShouldFail) {
+  rpc::GetUserHistoryRequest req;
+  rpc::GetUserHistoryReply res;
+
+  req.set_userid("User1");
+  cmd::GetUserHistory command(session());
+
+  auto status = command.doProcess(&req, &res);
+
+  ASSERT_FALSE(status.ok());
+
+  auto err = errorFromStatus(status);
+  ASSERT_EQ(err.code(), rpc::ErrorCode::USER_DOES_NOT_EXIST);
+}
+
+TEST_F(GetUserHistoryTest, NewUserHasNoHistory) {
+  rpc::GetUserHistoryRequest req;
+  rpc::GetUserHistoryReply res;
+  rpc::Error err;
+
+  constexpr auto username = "User1";
+
+  createUser(session(), username);
+
+  req.set_userid("User1");
+
+  cmd::GetUserHistory command(session());
+
+  ASSERT_TRUE(command.doProcess(&req, &res).ok());
+
+  ASSERT_EQ(0, res.events_size());
+}
+
+};  // namespace
