a50ffc93ca827d67ef0fd6487bc1e804427996e7
Andreas C. Osowski <andreas@osowski.de>
2018-05-04 10:00:36 +0200

    hub/service: AttachmentService separation of concerns

--- hub/service/attachment_service.cc
+++ hub/service/attachment_service.cc
@@ -13,6 +13,80 @@
 
 namespace hub {
 namespace service {
+
+bool AttachmentService::checkSweepTailsForConfirmation(
+    db::Connection& connection, const db::Sweep& sweep,
+    const std::vector<std::string>& tails) {
+  auto confirmedTails = _api->filterConfirmedTails(tails);
+
+  LOG(INFO) << "Sweep: " << sweep.id << " (" << sweep.bundleHash
+            << ") has: " << confirmedTails.size() << " confirmed tails.";
+
+  if (confirmedTails.size() == 1) {
+    const auto& tail = *confirmedTails.cbegin();
+    LOG(INFO) << "Marking tail as confirmed: " << tail;
+    db::markTailAsConfirmed(connection, tail);
+    return true;
+  } else if (confirmedTails.size() > 1) {
+    LOG(FATAL) << "More than one confirmed tail!!!";
+  }
+
+  return false;
+}
+
+bool AttachmentService::checkForUserReattachment(
+    db::Connection& connection, const db::Sweep& sweep,
+    const std::vector<std::string>& knownTails) {
+  auto bundleTransactionHashes =
+      _api->findTransactions({}, std::vector<std::string>{sweep.bundleHash});
+  auto bundleTransactions = _api->getTrytes(bundleTransactionHashes);
+
+  // Remove non-tails or tails that we know of
+  bundleTransactions.erase(
+      std::remove_if(bundleTransactions.begin(), bundleTransactions.end(),
+                     [&knownTails](const auto& tx) {
+                       return
+                           // Not a tail
+                           tx.currentIndex != 0 ||
+                           // Already know about this one
+                           std::find(knownTails.begin(), knownTails.end(),
+                                     tx.hash) != knownTails.end();
+                     }),
+      bundleTransactions.end());
+
+  if (bundleTransactions.size() != 0) {
+    // Third-party (i.e. user erattachments!)
+    std::vector<std::string> userTails;
+
+    std::transform(bundleTransactions.cbegin(), bundleTransactions.cend(),
+                   userTails.begin(), [](const auto& ref) { return ref.hash; });
+
+    auto confirmedTails = _api->filterConfirmedTails(userTails);
+    if (confirmedTails.size() != 0) {
+      // FIXME what if error logic more than one confirmed tail
+      const auto& tail = *confirmedTails.cbegin();
+      LOG(INFO) << "Inserting confirmed user-attached tail: " << tail;
+      db::createTail(connection, sweep.id, tail);
+
+      LOG(INFO) << "Marking tail as confirmed: " << tail;
+      db::markTailAsConfirmed(connection, tail);
+      return true;
+    }
+
+    // This also makes sure that we only add bundles that IRI has seen
+    // fully.
+    auto consistentTails = _api->filterConsistentTails(userTails);
+
+    if (consistentTails.size() != 0) {
+      for (const auto& tail : consistentTails) {
+        LOG(INFO) << "Inserting UNconfirmed user-attached tail: " << tail;
+        db::createTail(connection, sweep.id, tail);
+      }
+    }
+  }
+  return false;
+}
+
 bool AttachmentService::doTick() {
   // We're splitting processing out to separate methods on purpose.
   // This means, that we can't really batch API requests across sweeps.
@@ -31,100 +105,42 @@ bool AttachmentService::doTick() {
     auto unconfirmedSweeps = db::getUnconfirmedSweeps(connection, tickStart);
     LOG(INFO) << "Found " << unconfirmedSweeps.size() << " unconfirmed sweeps.";
 
-    // 2. Get (tails, timestamp) for these sweeps
     for (const auto& sweep : unconfirmedSweeps) {
-      auto sweepTails = db::getTailsForSweep(connection, sweep.id);
-      // sort sweepTails newest first.
-      std::sort(sweepTails.begin(), sweepTails.end(),
-                [](const auto& a, const auto& b) {
-                  return a.createdAt > b.createdAt;
-                });
+      // 2. Get (tails, timestamp) for these sweeps
+      {
+        const auto sweepTails = db::getTailsForSweep(connection, sweep.id);
 
-      // 3. Check if one of the tails is confirmed.
-      std::vector<std::string> tails;
-      std::transform(sweepTails.cbegin(), sweepTails.cend(), tails.begin(),
-                     [](const auto& tail) { return tail.hash; });
-      auto confirmedTails = _api->filterConfirmedTails(tails);
-
-      LOG(INFO) << "Sweep: " << sweep.id << " (" << sweep.bundleHash
-                << ") has: " << confirmedTails.size() << " confirmed tails.";
-
-      if (confirmedTails.size() == 1) {
-        const auto& tail = *confirmedTails.cbegin();
-        LOG(INFO) << "Marking tail as confirmed: " << tail;
-        db::markTailAsConfirmed(connection, tail);
-        continue;
-      } else if (confirmedTails.size() > 1) {
-        LOG(FATAL) << "More than one confirmed tail!!!";
-      }
-
-      // 4. If not, check if at least one of the tails per sweep is still
-      //    promotable
-
-      auto consistentTails = _api->filterConsistentTails(tails);
-      // 4.1. If yes, pick most recent and promote.
-      if (consistentTails.size() > 0) {
-        // Get newest tail.
-        auto newest = std::find_if(sweepTails.begin(), sweepTails.end(),
-                                   [&consistentTails](const auto& tail) {
-                                     return consistentTails.find(tail.hash) !=
-                                            consistentTails.end();
-                                   });
-
-        // TODO promote.
-      }
-
-      // 5. If not, check if a user reattachment was confirmed
-      auto bundleTransactionHashes = _api->findTransactions(
-          {}, std::vector<std::string>{sweep.bundleHash});
-      auto bundleTransactions = _api->getTrytes(bundleTransactionHashes);
-
-      // Remove non-tails or tails that we know of
-      bundleTransactions.erase(
-          std::remove_if(bundleTransactions.begin(), bundleTransactions.end(),
-                         [&tails](const auto& tx) {
-                           return
-                               // Not a tail
-                               tx.currentIndex != 0 ||
-                               // Already know about this one
-                               std::find(tails.begin(), tails.end(), tx.hash) !=
-                                   tails.end();
-                         }),
-          bundleTransactions.end());
-
-      if (bundleTransactions.size() != 0) {
-        // Third-party (i.e. user erattachments!)
-        std::vector<std::string> userTails;
-
-        std::transform(bundleTransactions.cbegin(), bundleTransactions.cend(),
-                       userTails.begin(),
-                       [](const auto& ref) { return ref.hash; });
-
-        auto confirmedTails = _api->filterConfirmedTails(userTails);
-        if (confirmedTails.size() != 0) {
-          // FIXME what if error logic more than one confirmed tail
-          const auto& tail = *confirmedTails.cbegin();
-          LOG(INFO) << "Inserting confirmed user-attached tail: " << tail;
-          db::createTail(connection, sweep.id, tail);
-
-          LOG(INFO) << "Marking tail as confirmed: " << tail;
-          db::markTailAsConfirmed(connection, tail);
+        // 3. Check if one of the tails is confirmed.
+        if (checkSweepTailsForConfirmation(connection, sweep, sweepTails)) {
           continue;
         }
 
-        // This also makes sure that we only add bundles that IRI has seen fully.
-        auto consistentTails = _api->filterConsistentTails(userTails);
+        // 4. If not, check if a user reattachment was confirmed
+        checkForUserReattachment(connection, sweep, sweepTails);
+      }
 
-        if (consistentTails.size() != 0) {
-          for (const auto& tail : consistentTails) {
-            LOG(INFO) << "Inserting UNconfirmed user-attached tail: " << tail;
-            db::createTail(connection, sweep.id, tail);
-          }
+      // 5. If not, check if at least one of the tails per sweep is still
+      //    promotable
+      {
+        // Requerying list of tails because `checkForUserReattachment` might
+        // have added some.
+        const auto sweepTails = db::getTailsForSweep(connection, sweep.id);
+        auto consistentTails = _api->filterConsistentTails(sweepTails);
+        // 5.1. If yes, pick most recent and promote.
+        if (consistentTails.size() > 0) {
+          // Get newest tail.
+          auto toPromote = std::find_if(sweepTails.begin(), sweepTails.end(),
+                                        [&consistentTails](const auto& tail) {
+                                          return consistentTails.find(tail) !=
+                                                 consistentTails.end();
+                                        });
+
+          // TODO(th0br0): promote.
         }
       }
 
       // 6. If not, reattach and commit tail to DB.
-      // TODO reattach
+      // TODO(th0br0): reattach
       LOG(INFO) << "Sweep " << sweep.id << " is still unconfirmed.";
     }
 
--- hub/service/attachment_service.h
+++ hub/service/attachment_service.h
@@ -7,11 +7,18 @@
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
+#include "hub/db/db.h"
 #include "hub/iota/api.h"
 #include "hub/service/scheduled_service.h"
 
 namespace hub {
+namespace db {
+struct Sweep;
+struct SweepTail;
+}  // namespace db
+
 namespace service {
 
 class AttachmentService : public ScheduledService {
@@ -25,6 +32,15 @@ class AttachmentService : public ScheduledService {
 
  protected:
   bool doTick() override;
+
+ protected:
+  bool checkForUserReattachment(db::Connection& connection,
+                                const db::Sweep& sweep,
+                                const std::vector<std::string>& knownTails);
+
+  bool checkSweepTailsForConfirmation(db::Connection& connection,
+                                      const db::Sweep& sweep,
+                                      const std::vector<std::string>& tails);
 
  protected:
   const std::shared_ptr<hub::iota::IotaAPI> _api;
