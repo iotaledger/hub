e5d8b80ede471719cdef72744eec6fcd899c2834
Andreas C. Osowski <andreas@osowski.de>
2018-08-02 16:14:38 +0200

    hub/service: fix for entangled changes

--- hub/service/tests/address_monitor.cc
+++ hub/service/tests/address_monitor.cc
@@ -40,8 +40,9 @@ class MockAPI : public cppclient::IotaAPI {
 
   MOCK_METHOD0(isNodeSolid, bool());
 
-  MOCK_METHOD1(getBalances, std::unordered_map<std::string, uint64_t>(
-                                const std::vector<std::string>& addresses));
+  MOCK_METHOD1(getBalances,
+               nonstd::optional<std::unordered_map<std::string, uint64_t>>(
+                   const std::vector<std::string>& addresses));
 
   MOCK_METHOD1(getConfirmedBundlesForAddresses,
                std::unordered_multimap<std::string, Bundle>(
@@ -129,14 +130,16 @@ TEST_F(AddressMonitorTest, Tick) {
   EXPECT_CALL(api, getBalances(std::vector<std::string>{address}))
       .Times(1)
       .WillOnce(
-          Return(std::unordered_map<std::string, uint64_t>{{address, 0}}));
+          Return(nonstd::optional<std::unordered_map<std::string, uint64_t>>{
+              std::unordered_map<std::string, uint64_t>{{address, 0}}}));
 
   monitor.doTick();
 
   EXPECT_CALL(api, getBalances(std::vector<std::string>{address}))
       .Times(1)
       .WillOnce(
-          Return(std::unordered_map<std::string, uint64_t>{{address, 1000}}));
+          Return(nonstd::optional<std::unordered_map<std::string, uint64_t>>{
+              std::unordered_map<std::string, uint64_t>{{address, 1000}}}));
 
   // Test new balance arrival
   std::vector<AddressMonitor::BalanceChange> ex1 = {{0, address, 1000, 1000}};
@@ -150,7 +153,8 @@ TEST_F(AddressMonitorTest, Tick) {
   EXPECT_CALL(api, getBalances(std::vector<std::string>{address}))
       .Times(3)
       .WillRepeatedly(
-          Return(std::unordered_map<std::string, uint64_t>{{address, 0}}));
+          Return(nonstd::optional<std::unordered_map<std::string, uint64_t>>{
+              std::unordered_map<std::string, uint64_t>{{address, 0}}}));
 
   // Update is refused
   std::vector<AddressMonitor::BalanceChange> ex2 = {{0, address, 0, -1000}};
@@ -220,8 +224,10 @@ TEST_F(AddressMonitorTest, RemoveUnmonitoredAddresses) {
       .WillOnce(Return(monitored));
   EXPECT_CALL(api, getBalances(_))
       .Times(1)
-      .WillOnce(Return(std::unordered_map<std::string, uint64_t>{
-          {addressA, 10}, {addressB, 20}}));
+      .WillOnce(
+          Return(nonstd::optional<std::unordered_map<std::string, uint64_t>>{
+              std::unordered_map<std::string, uint64_t>{{addressA, 10},
+                                                        {addressB, 20}}}));
 
   std::unordered_map<uint64_t, uint64_t> balancesAfter = {{0, 10}, {1, 20}};
 
