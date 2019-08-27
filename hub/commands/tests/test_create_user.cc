9d3588486c00f8ac48185f972053cfed2378ec7d
Tsvi <tsvi.sabo@iota.org>
2019-08-27 16:23:07 +0300

    rpchub/commands/tests: create_user - remove unused

--- hub/commands/tests/test_create_user.cc
+++ hub/commands/tests/test_create_user.cc
@@ -20,9 +20,6 @@ namespace {
 class CreateUserTest : public CommandTest {};
 
 TEST_F(CreateUserTest, ErrorOnDuplicate) {
-  db::sql::UserAccount tbl;
-
-  auto& conn = hub::db::DBManager::get().connection();
 
   createUser(session(), "User1");
   auto status = createUser(session(), "User1");
@@ -30,22 +27,13 @@ TEST_F(CreateUserTest, ErrorOnDuplicate) {
 
   ASSERT_EQ(grpc::StatusCode::FAILED_PRECONDITION, status.error_code());
   ASSERT_EQ(err.code(), rpc::ErrorCode::USER_EXISTS);
-  /*  ASSERT_EQ(1,
-     conn(select(count(tbl.identifier)).from(tbl).unconditionally()) .front()
-                   .count);*/
+
 }
 
 TEST_F(CreateUserTest, CreateUsers) {
-  db::sql::UserAccount tbl;
-
-  auto& conn = hub::db::DBManager::get().connection();
 
   ASSERT_TRUE(createUser(session(), "User1").ok());
   ASSERT_TRUE(createUser(session(), "User2").ok());
-
-  /*ASSERT_EQ(2, conn(select(count(tbl.identifier)).from(tbl).unconditionally())
-                   .front()
-                   .count);*/
 }
 
 };  // namespace
