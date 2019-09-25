c3570e170ebc55ab2213342e42f01bbb3e754d5e
Tsvi <tsvi.sabo@iota.org>
2019-09-25 13:42:22 +0300

    rpchub -> hub

--- common/crypto/provider_base.h
+++ common/crypto/provider_base.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018 IOTA Stiftung
- * https://github.com/iotaledger/rpchub
+ * https://github.com/iotaledger/hub
  *
  * Refer to the LICENSE file for licensing information
  */
