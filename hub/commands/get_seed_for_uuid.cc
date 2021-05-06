beef796f4ea4375f7744842c37f76af1fe69035d
bingyanglin <bingyang.lin@iota.org>
2021-05-06 21:50:20 +0800

    Add GetSeedForUUID HTTP Rest API

---
+++ hub/commands/get_seed_for_uuid.cc
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2019 IOTA Stiftung
+ * https://github.com/iotaledger/hub
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#include <cstdint>
+
+#include "common/converter.h"
+#include "common/crypto/manager.h"
+#include "common/crypto/types.h"
+#include "hub/commands/factory.h"
+#include "hub/commands/helper.h"
+#include "hub/db/helper.h"
+
+#include "hub/commands/get_seed_for_uuid.h"
+
+namespace hub {
+namespace cmd {
+
+static CommandFactoryRegistrator<GetSeedForUUID> registrator;
+
+boost::property_tree::ptree GetSeedForUUID::doProcess(
+    const boost::property_tree::ptree& request) noexcept {
+  boost::property_tree::ptree tree;
+  GetSeedForUUIDRequest req;
+  GetSeedForUUIDReply rep;
+
+  auto maybeUserId = request.get_optional<std::string>("userId");
+  if (!maybeUserId) {
+    tree.add("error",
+             common::cmd::getErrorString(common::cmd::MISSING_ARGUMENT));
+    return tree;
+  }
+
+  req.userId = maybeUserId.value();
+
+  auto status = doProcess(&req, &rep);
+
+  if (status != common::cmd::OK) {
+    tree.add("error", common::cmd::getErrorString(status));
+  } else {
+    tree.add("seed", rep.seed);
+  }
+  return tree;
+}
+
+common::cmd::Error GetSeedForUUID::doProcess(
+    const GetSeedForUUIDRequest* request,
+    GetSeedForUUIDReply* response) noexcept {
+  auto& connection = db::DBManager::get().connection();
+
+  // Get userId for identifier
+  {
+    auto maybeUserId = connection.userIdFromIdentifier(request->userId);
+    if (!maybeUserId) {
+      return common::cmd::USER_DOES_NOT_EXIST;
+    }
+  }
+
+  response->seed =
+      common::crypto::CryptoManager::get().provider().getSeedFromUUID(
+          common::crypto::UUID(request->userId));
+
+  return common::cmd::OK;
+}
+}  // namespace cmd
+}  // namespace hub
---
+++ hub/commands/get_seed_for_uuid.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019 IOTA Stiftung
+ * https://github.com/iotaledger/hub
+ *
+ * Refer to the LICENSE file for licensing information
+ */
+
+#ifndef HUB_COMMANDS_GET_SEED_FOR_UUID_H_
+#define HUB_COMMANDS_GET_SEED_FOR_UUID_H_
+
+#include <string>
+
+#include "common/commands/command.h"
+
+namespace hub {
+namespace cmd {
+
+typedef struct GetSeedForUUIDRequest {
+  std::string userId;
+} GetSeedForUUIDRequest;
+
+typedef struct GetSeedForUUIDReply {
+  std::string seed;
+} GetSeedForUUIDReply;
+
+class GetSeedForUUID
+    : public common::Command<GetSeedForUUIDRequest, GetSeedForUUIDReply> {
+ public:
+  using Command<GetSeedForUUIDRequest, GetSeedForUUIDReply>::Command;
+
+  static std::shared_ptr<common::ICommand> create() {
+    return std::shared_ptr<common::ICommand>(
+        new GetSeedForUUID(std::make_shared<common::ClientSession>()));
+  }
+
+  static const std::string name() { return "GetSeedForUUID"; }
+
+  common::cmd::Error doProcess(const GetSeedForUUIDRequest* request,
+                               GetSeedForUUIDReply* response) noexcept override;
+
+  boost::property_tree::ptree doProcess(
+      const boost::property_tree::ptree& request) noexcept override;
+};
+}  // namespace cmd
+}  // namespace hub
+
+#endif  // HUB_COMMANDS_GET_SEED_FOR_UUID_H_
